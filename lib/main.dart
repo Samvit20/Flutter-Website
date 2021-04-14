d2deecf653a8855d08a8064f2c18331c4108ce7c
Samvit.S <samvit999@gmail.com>
2021-04-14 21:45:45 +0530

    Flutter Website

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Again',
+      title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Montserrat"),
       home: MyHomePage(),
     );
